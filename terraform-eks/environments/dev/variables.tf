830dbd86ca15fd3b95cb20325535763c916abb67
azurebose <108393935+azurebose@users.noreply.github.com>
2025-06-17 10:46:05 +0530

    Update variables.tf

--- terraform-eks/environments/dev/variables.tf
+++ terraform-eks/environments/dev/variables.tf
@@ -1,6 +1,6 @@
-variable "aws_region" { default = "us-west-2" }
+variable "aws_region" { default = "us-east-1" }
 variable "name" { default = "dev" }
 variable "vpc_cidr" { default = "10.0.0.0/16" }
 variable "public_subnet_cidrs" { default = ["10.0.1.0/24", "10.0.2.0/24"] }
-variable "azs" { default = ["us-west-2a", "us-west-2b"] }
+variable "azs" { default = ["us-east-2a", "us-east-2b"] }
 variable "cluster_name" { default = "dev-eks-cluster" }
